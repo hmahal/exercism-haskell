4d9a7ee21279f049353884fc1b1913533c1bff83
haro-freezd <harmanmahal@gmail.com>
2018-06-02 01:57:39 -0700

    init

---
+++ leap/src/LeapYear.hs
@@ -0,0 +1,6 @@
+module LeapYear (isLeapYear) where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year = 
+  if year `mod` 100 == 0 && year `mod` 400 /= 0 then False
+    else if year `mod` 4 == 0 then True else False
